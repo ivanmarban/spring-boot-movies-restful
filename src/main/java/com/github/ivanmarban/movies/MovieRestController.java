ef5ea25905d151c7c63633db7ed8b83bf8558926
Ivan Marban <ivan.marban@gmail.com>
2015-03-30 22:59:18 +0200

    REST controller

---
+++ src/main/java/com/github/ivanmarban/movies/MovieRestController.java
@@ -0,0 +1,50 @@
+package com.github.ivanmarban.movies;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieRestController {
+
+	@Autowired
+	private MovieRepository repo;
+
+	@RequestMapping(value = "api/v1/movies", method = RequestMethod.GET)
+	public List<Movie> getAll() {
+		return repo.findAll();
+	}
+	
+	@RequestMapping(value = "api/v1/movie/{id}", method = RequestMethod.GET)
+	public Movie get(@PathVariable String id) {
+		return repo.findOne(id);
+	}
+
+	@RequestMapping(value = "api/v1/movie", method = RequestMethod.POST)
+	public Movie create(@RequestBody Movie movie) {
+		return repo.save(movie);
+	}
+
+	@RequestMapping(value = "api/v1/movie/{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable String id) {
+		repo.delete(id);
+	}
+
+	@RequestMapping(value = "api/v1/movie/{id}", method = RequestMethod.PUT)
+	public Movie update(@PathVariable String id, @RequestBody Movie movie) {
+		Movie update = repo.findOne(id);
+		update.setDirector(movie.getDirector());
+		update.setGenre(movie.getGenre());
+		update.setRated(movie.getRated());
+		update.setRuntime(movie.getRuntime());
+		update.setTitle(movie.getTitle());
+		update.setYear(movie.getYear());
+		return repo.save(update);
+	}
+
+}
