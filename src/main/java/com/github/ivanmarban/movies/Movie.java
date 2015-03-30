e874b5ed0e40432053ac4a9b701f47001e634136
Ivan Marban <ivan.marban@gmail.com>
2015-03-30 22:58:32 +0200

    Movie java object

---
+++ src/main/java/com/github/ivanmarban/movies/Movie.java
@@ -0,0 +1,74 @@
+package com.github.ivanmarban.movies;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "movies")
+public class Movie {
+
+	@Id
+	private String id;
+	private String title;
+	private String year;
+	private String rated;
+	private String runtime;
+	private String genre;
+	private String director;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getRated() {
+		return rated;
+	}
+
+	public void setRated(String rated) {
+		this.rated = rated;
+	}
+
+	public String getRuntime() {
+		return runtime;
+	}
+
+	public void setRuntime(String runtime) {
+		this.runtime = runtime;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+}
