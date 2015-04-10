fe571d40ac90e71f789a5ab726cbaea0c992ec9d
Ivan Marban <ivan.marban@gmail.com>
2015-04-10 23:54:09 +0200

    Added PUT method to CORS filter

--- src/main/java/com/github/ivanmarban/movies/MovieRestCORSFilter.java
+++ src/main/java/com/github/ivanmarban/movies/MovieRestCORSFilter.java
@@ -16,7 +16,7 @@ public class MovieRestCORSFilter implements Filter {
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		chain.doFilter(req, res);
